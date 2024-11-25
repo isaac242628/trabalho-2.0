7884fca9de273ca8a011e912d26894bc0b176eb8
Isaac <isaac.souza26@etec.gov.br>
2024-11-25 00:08:21 -0300

    deu certooo

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let itens = document.querySelectorAll(".item");
 
-        // Função de scroll
-        document.addEventListener("scroll", () => {
-            // Calcula a posição do carrossel em relação à tela
-            let proporcao = document.querySelector(".carrossel").getBoundingClientRect().top / window.innerHeight;
+// Função de scroll
+document.addEventListener("scroll", () => {
+  // Calcula a posição do carrossel em relação à tela
+  let proporcao =
+    document.querySelector(".carrossel").getBoundingClientRect().top /
+    window.innerHeight;
 
-            // Calculando o índice do item ativo
-            let index = Math.max(0, Math.min(itens.length - 1, Math.ceil(-1 * (proporcao + 0.5))));
+  // Calculando o índice do item ativo
+  let index = Math.max(
+    0,
+    Math.min(itens.length - 1, Math.ceil(-1 * (proporcao + 0.5)))
+  );
 
-            // Atualizando os itens de acordo com o índice
-            itens.forEach((item, i) => {
-                if (i === index) {
-                    item.classList.add("active");
-                } else {
-                    item.classList.remove("active");
-                }
-            });
-        });
+  // Atualizando os itens de acordo com o índice
+  itens.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add("active");
+    } else {
+      item.classList.remove("active");
+    }
+  });
+});
