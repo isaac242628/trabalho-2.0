a716dd0bc956b6a24ef374c134d757abe6bfffa4
isaac242628 <isaac.souza26@etec.sp.gov.br>
2024-11-24 23:40:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let itens = document.querySelectorAll(".item");
+
+        // Função de scroll
+        document.addEventListener("scroll", () => {
+            // Calcula a posição do carrossel em relação à tela
+            let proporcao = document.querySelector(".carrossel").getBoundingClientRect().top / window.innerHeight;
+
+            // Calculando o índice do item ativo
+            let index = Math.max(0, Math.min(itens.length - 1, Math.ceil(-1 * (proporcao + 0.5))));
+
+            // Atualizando os itens de acordo com o índice
+            itens.forEach((item, i) => {
+                if (i === index) {
+                    item.classList.add("active");
+                } else {
+                    item.classList.remove("active");
+                }
+            });
+        });
